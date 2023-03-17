67d0074731a045e51df8b95a75e20f88f0e0f53c
APSDIGIT\mpotet <maxime.potet@apside-groupe.com>
2023-03-17 12:10:40 +0100

    Correction test

--- todolist-debut-ex2/src/test/java/com/acme/todolist/ControllerTests/TodoListControllerTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/ControllerTests/TodoListControllerTests.java
@@ -5,6 +5,8 @@ import com.acme.todolist.adapters.persistence.TodoItemRepository;
 import com.acme.todolist.application.service.GetTodoItemsService;
 import com.acme.todolist.configuration.TodolistApplication;
 import com.acme.todolist.domain.TodoItem;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,23 +17,22 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.Instant;
-import java.util.Objects;
+import java.util.List;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
-        classes = TodolistApplication.class)
+@SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(
-        locations = "classpath:application-integrationtest.properties")
 public class TodoListControllerTests {
     @Autowired
     private TodoItemRepository todoItemRepository;
@@ -41,6 +42,9 @@ public class TodoListControllerTests {
 
     @Autowired
     private TodoItemMapper mapper;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     private void createTestTodo(String id, Instant instant, String content){
         TodoItem todoItem = new TodoItem(id, instant, content);
@@ -52,19 +56,26 @@ public class TodoListControllerTests {
         createTestTodo("testItemMore24", Instant.now().plusSeconds(3600 * 25), "Test item de plus de 24h de retard");
         createTestTodo("testItemEqual24", Instant.now().plusSeconds(3600 * 24), "Test item d'exactement 24h de retard");
         createTestTodo("testItemMinus24", Instant.now().plusSeconds(3600 * 23), "Test item de moins de 24h de retard");
-        getTodoItemsService.getAllTodoItems().forEach(todoItem -> {
-            switch (todoItem.getId()) {
+        // When
+        MvcResult mvcResult = mockMvc.perform(get("/todo-items"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // Then
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<TodoItem> todoItems = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<List<TodoItem>>() {});
+        for(TodoItem todoItem : todoItems) {
+            switch(todoItem.getId()) {
                 case "testItemMore24":
-                    Assert.assertTrue(todoItem.getContent().startsWith("[LATE!] "));
+                    assertTrue(todoItem.getContent().startsWith("[LATE!] "));
                     break;
                 case "testItemEqual24":
-                    Assert.assertFalse(todoItem.getContent().startsWith("[LATE!] "));
-                    break;
                 case "testItemMinus24":
-                    Assert.assertFalse(todoItem.getContent().startsWith("[LATE!] "));
+                    assertFalse(todoItem.getContent().startsWith("[LATE!] "));
                     break;
                 default:
+                    fail("Unexpected todo item with id " + todoItem.getId());
             }
-        });
+        }
     }
 }
